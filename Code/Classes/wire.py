28b8679822398af7ab704ca51ded213ae4f7c021
martijnreus <martijn.reus@gmail.com>
2023-06-07 12:30:40 +0200

    Added a class for a complete wire as alternative for the network class

--- Code/Classes/wire.py
+++ Code/Classes/wire.py
@@ -1,3 +1,4 @@
+# create a network that keeps track of all the wires
 class Network:
 
     def __init__(self):
@@ -7,7 +8,7 @@ class Network:
         self.network[f"{gateA}-{gateB}"] = []
 
     def add_wire_part(self, gateA, gateB, location, direction):
-        self.network[f"{gateA}-{gateB}"].append(Wire(location, direction, 1))
+        self.network[f"{gateA}-{gateB}"].append(WireUnit(location, direction))
 
     def remove_wire_part(self, gateA, gateB):
         return self.network[f"{gateA}-{gateB}"].pop()
@@ -15,11 +16,28 @@ class Network:
     def check_is_connected(self, gateA, gateB):
         #TODO check if there is a completed wire between gateA and gateB and return true if there is
         return True
-
-# create a wire, keeping track of length and the dd
+    
+# created a wire between two gates
 class Wire:
 
-    def __init__(self, from_location, to_location, length):
+    def __init__(self, gateA, gateB):
+        self.gateA = gateA
+        self.gaetB = gateB
+        self.wires = []
+
+    def add_wire_part(self, from_location, to_location):
+        self.wires.append(WireUnit(from_location, to_location))
+
+    def remove_wire_part(self):
+        return self.wires.pop()
+    
+    def check_is_connected(self):
+        #TODO check if there is a completed wire between gateA and gateB and return true if there is
+        return True
+
+# create a wire, keeping track of length and the dd
+class WireUnit:
+
+    def __init__(self, from_location, to_location):
         self.from_location = from_location
         self.to_location = to_location
-        self.length = length
