b0bfdc36ae530fbb101aac3bbb42bff79314da9d
martijnreus <martijn.reus@gmail.com>
2023-06-08 09:59:46 +0200

    Updated wire.py where I removed Network class and added a functoin to return the length of the wire

--- Code/Classes/wire.py
+++ Code/Classes/wire.py
@@ -1,23 +1,3 @@
-
-# create a network that keeps track of all the wires
-class Network:
-
-    def __init__(self):
-        self.network = {}
-
-    def create_new_wire(self, gateA, gateB):
-        self.network[f"{gateA}-{gateB}"] = []
-
-    def add_wire_part(self, gateA, gateB, location, direction):
-        self.network[f"{gateA}-{gateB}"].append(WireUnit(location, direction))
-
-    def remove_wire_part(self, gateA, gateB):
-        return self.network[f"{gateA}-{gateB}"].pop()
-    
-    def check_is_connected(self, gateA, gateB):
-        #TODO check if there is a completed wire between gateA and gateB and return true if there is
-        return True
-    
 # created a wire between two gates
 class Wire:
 
@@ -34,7 +14,11 @@ class Wire:
     
     def check_is_connected(self):
         #TODO check if there is a completed wire between gateA and gateB and return true if there is
-        return True
+        if (self.wires[len(self.wires - 1)].to_location):
+            return True
+    
+    def get_wire_length(self):
+        return len(self.wires)
 
 # create a wire, keeping track of length and the dd
 class WireUnit:
