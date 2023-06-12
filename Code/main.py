d58d220842eb0b8ba796160216377bdf4bbfa863
Anna-lytic <a.stegmann98@gmail.com>
2023-06-12 13:56:14 +0200

    main spaces

--- Code/main.py
+++ Code/main.py
@@ -12,11 +12,15 @@ from plot import *
 sys.path.append("algorithms")
 from greedy import *
 from randomize import *
+
+# choose the algorithm based on the arguments
 def choose_algorithm(algorithm, chip):
     if algorithm == "greedy":
         greedy_algorithm(chip.wires, chip.wire_connections)
     elif algorithm == "random":
         random_algorithm(chip.wires, chip.wire_connections, chip.grid, chip.gates)
+
+# main
 def main():
 
     # Check command line arguments
