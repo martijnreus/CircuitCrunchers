023882848833d0734ed71cc0ca294ab584fe131a
Juri Dijkstra <juri_bd@sp-byods-145-109-45-66.wireless.uva.nl>
2023-06-12 12:25:19 +0200

    added some dijkstra code

--- Code/Classes/grid.py
+++ Code/Classes/grid.py
@@ -5,3 +5,10 @@ class Grid:
         self.width = width
         self.height = height
         self.layers = layers
+
+    def get_max_locations(self):
+        max_x = self.width - 1
+        max_y = self.height - 1
+        max_z = self.layers - 1
+        xyz = [max_x, max_y, max_z]
+        return xyz
