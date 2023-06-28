fb46cd0f7d623d571120547bf63492d46a768b7f
Anna-lytic <a.stegmann98@gmail.com>
2023-06-28 14:08:36 +0200

    style: docstrings en comments

--- Code/Classes/testing.py
+++ Code/Classes/testing.py
@@ -1,3 +1,16 @@
+###################################################
+# Testing algotithm, to test algorithms and order
+#
+# options: 
+# 1. test all netlists/ only one specific
+# 2. which algorithm to test
+# 3. which sorting order
+# 
+# algorithms that are based on randomness will be 
+# tested n times
+###################################################
+
+# import from libraries
 import sys
 import csv
 import os
@@ -22,49 +35,113 @@ from plot import *
 
 # Test algorithms and order
 class Testing():
+    """
+    Class for performing testing on algorithms and sorting orders.
+    """
 
     # make empty lists and libraries
     def __init__(self, subject, algorithm, number_netlist, order_choice, order) -> None:
+        """
+        Initialize the Testing object.
 
+        Args:
+            subject (str): The subject of the testing (either "order" or "algorithm").
+            algorithm (str): The algorithm to be tested.
+            number_netlist (str): The number of the netlist to be tested.
+            order_choice (str): The choice of sorting order for testing algorithms.
+            order (str): The specific sorting order to be used (valid for algorithm testing).
+
+        Preconditions:
+            - subject must be either "order" or "algorithm".
+            - algorithm must be a valid algorithm name.
+            - number_netlist must be a valid netlist number.
+            - order_choice must be a valid sorting order name.
+            - order must be a valid sorting order name (applicable only for algorithm testing).
+
+        """
+
+        # initialize class variables to define test
         self.subject = subject
         self.algorithm = algorithm
-        self.netlist = f"netlist_{int(number_netlist)}"
         self.order_choice = order_choice
         self.order = order
+
+        # get the info for the chip
+        self.netlist = f"netlist_{int(number_netlist)}"
         self.chip_id = f"{get_number_chip(number_netlist)}"
         self.gates_file = f"print_{get_number_chip(number_netlist)}"
+
+        # get title
         self.title = f"{subject}_{self.netlist}_{algorithm}_{order}"
+        
+        # initialize empty variables
         self.cost_list = []
         self.netlists = []
         self.variables = []
 
-
+    # delete the current csv
     def delete_csv(self):
+        """
+        Delete the CSV file associated with the current testing.
+
+        Postconditions:
+            - The CSV file for the current testing is deleted if it exists.
+
+        """
         filepath = f'output/{self.title}.csv'
         if not os.path.exists(filepath):
             pass
         else:
             os.remove(filepath)
 
-
     # output to csv
     def make_csv(self, filepath):
+        """
+        Create a new CSV file for writing test results.
 
+        Args:
+            filepath (str): The path of the CSV file.
+
+        Preconditions:
+            - The directory structure for the given filepath must exist.
+
+        """
         with open(filepath, 'w', newline='') as newfile:
             writer = csv.writer(newfile, dialect='excel')
             writer.writerow(["test", "cost"])
 
-
+    # get the filepath for the csv
     def get_filepath(self, title):
+        """
+        Get the file path for storing test results.
+
+        Args:
+            title (str): The title of the test.
+
+        Returns:
+            str: The file path for storing test results.
+
+        """
         if self.subject == "order":
             filepath = f'output/{self.subject}/{title}'
         else:
             filepath = f'output/{self.subject}/{self.algorithm}/{title}'
         return filepath
 
-
+    # write one line to csv
     def write_to_csv(self,title,info):
+        """
+        Write the test information to a CSV file.
 
+        Args:
+            title (str): The title of the test.
+            info (list): The information to be written to the CSV file.
+
+        Preconditions:
+            - The directory structure for the given filepath must exist.
+            - The CSV file with the given title must exist or be created.
+
+        """
         filepath = self.get_filepath(title) + ".csv"
 
         if not os.path.exists(filepath):
@@ -85,9 +162,15 @@ class Testing():
     # test the orders for a netlist and algorithm
     def testing_order(self,algorithm):
         """
-        Automated testing
-        """
+        Perform automated testing for different sorting orders.
 
+        Args:
+            algorithm (str): The algorithm to be tested.
+
+        Preconditions:
+            - algorithm must be a valid algorithm name.
+
+        """
         sorting_orders = ["basic","reverse", "short", "long","least-connections", "most-connections", "sum-lowest", "sum-highest", "middle", "outside", "intra-quadrant", "inter-quadrant", "manhattan","x","y","x-rev","y,rev", "weighted", "weighted-rev"]
 
         self.variables = sorting_orders
@@ -110,7 +193,18 @@ class Testing():
 
     # get the average of the random order
     def testing_random_order(self, algorithm, n):
+        """
+        Perform automated testing for the random order algorithm.
 
+        Args:
+            algorithm (str): The algorithm to be tested.
+            n (int): The number of times the test should be run.
+
+        Preconditions:
+            - algorithm must be a valid algorithm name.
+            - n must be a positive integer.
+
+        """
         # loop to run the test n amount of times
         for number in range(n):
 
@@ -127,10 +221,20 @@ class Testing():
             info = [f"random_try_{number}", cost]
             self.write_to_csv(self.title, info)
 
-
     # get the average of some algorithms
     def average(self,n, algorithm):
+        """
+        Perform automated testing for average cost of algorithms.
 
+        Args:
+            n (int): The number of times the test should be run.
+            algorithm (str): The algorithm to be tested.
+
+        Preconditions:
+            - n must be a positive integer.
+            - algorithm must be a valid algorithm name.
+
+        """
         self.variables = list(range(n))
 
         # for the random algorithm
@@ -145,7 +249,6 @@ class Testing():
                 info = [f"random_try_{number}", cost]
                 self.write_to_csv(self.title, info)
 
-
         # for the 2D random algorithm
         elif algorithm == "random2D":
             for number in range(n):
@@ -158,8 +261,20 @@ class Testing():
                 info = [f"random2D_try_{number}", cost]
                 self.write_to_csv(self.title, info)
 
+    # test the hillclimber algorithms
     def hillclimber(self,n,algorithm):
+        """
+        Perform automated testing for hillclimber algorithm.
 
+        Args:
+            n (int): The number of times the test should be run.
+            algorithm (str): The algorithm to be tested.
+
+        Preconditions:
+            - n must be a positive integer.
+            - algorithm must be a valid algorithm name.
+
+        """
         # for the hillclimber
         if algorithm == "hillclimber":
 
@@ -168,9 +283,6 @@ class Testing():
             chip.load_gates()
             chip.load_netlist()
             hillclimber_algorithm(chip, n)
-            # for cost in cost_list:
-            #     info = ["better", cost]
-            #     self.write_to_csv(f"{self.title}",info)
         
         # for the annealing
         elif algorithm == "annealing":
@@ -180,53 +292,83 @@ class Testing():
             chip.load_gates()
             chip.load_netlist()
             simulated_annealing(chip, n)
-            # for cost in cost_list:
-            #     info = ["better", cost]
-            #     self.write_to_csv(f"{self.title}",info)
 
     # test the algorithms that always give the same answer
     def only_once(self,algorithm, order):
+        """
+        Perform automated testing for algorithms that give the same answer.
 
-        title = f"{algorithm}_{self.netlist}"
+        Args:
+            algorithm (str): The algorithm to be tested.
+            order (str): The sorting order to be used.
 
+        Preconditions:
+            - algorithm must be a valid algorithm name.
+            - order must be a valid sorting order name.
+
+        """
+        # if astar
         if algorithm == "astar":
-
+            
+            # define versions
             versions = ["optimal", "avoid_center", "avoid_gates", "avoid_both", "normal"]
 
+            # run all versions
             for version in versions:
 
+                # load new chip
                 chip = Chip(self.chip_id, self.netlist, self.gates_file)
-                # load everything
                 chip.load_gates()
                 chip.load_netlist()
 
+                # run algorithm and calulate cost
                 astar_algorithm(chip,version)
                 cost = chip.calculate_cost()
+
+                # print, visualize and produce output
                 print(f"{version} cost: {cost}")
                 info = [f"astar_algorithm_{version}_{order}", cost]
                 self.write_to_csv(self.title, info)
 
         else:
-
-            # astar
+            # load new chip
             chip = Chip(self.chip_id, self.netlist, self.gates_file)
-            # load everything
             chip.load_gates()
             chip.load_netlist()
+
+            # run algorithm and calulate cost
             choose_algorithm(algorithm, chip, order)
             cost = chip.calculate_cost()
+
+            # print, visualize and produce output
             print(f"cost: {cost}")
-            visualize(chip, title)
+            visualize(chip, self.title)
             info = [f"{algorithm}_algorithm_{order}", cost]
             self.write_to_csv(self.title, info)
-
-
-            
+  
 # main test function
 def test(subject, algorithm, number_netlist, order_choice, n):
+    """
+    Perform the main test based on the subject, algorithm, and options chosen.
 
+    Args:
+        subject (str): The subject of the test.
+        algorithm (str): The algorithm to be tested.
+        number_netlist (str): The number of the netlist to be tested.
+        order_choice (str): The choice of sorting order.
+        n (int): The number of times the test should be run.
+
+    Preconditions:
+        - subject must be a valid subject name.
+        - algorithm must be a valid algorithm name.
+        - number_netlist must be a valid netlist number.
+        - order_choice must be a valid order choice.
+        - n must be a positive integer.
+
+    """
     order = "all"
 
+    # input sorting order
     if algorithm in ["astar", "greedy", "hillclimber"] and subject == "algorithm":
         order = input("Sorting order: ")
 
@@ -238,10 +380,16 @@ def test(subject, algorithm, number_netlist, order_choice, n):
 
         netlists = ["1","2","3","4","5","6","7","8","9"]
 
+        # for every netlist
         for number_netlist in netlists:
+
             # make new test object
             testing = Testing(subject, algorithm, number_netlist, order_choice, order)
+            
+            # delete old csv
             testing.delete_csv()
+
+            # start testing
             print(f"---chip: {testing.chip_id} and netlist: {number_netlist}-------------------")
             choose_test(testing, n, order)
 
@@ -249,16 +397,34 @@ def test(subject, algorithm, number_netlist, order_choice, n):
     else:
         # make new test object
         testing = Testing(subject, algorithm, number_netlist, order_choice, order)
+        
+        # delete old csv
         testing.delete_csv()
+
+        # start testing
         print(f"---chip: {testing.chip_id} and netlist: {number_netlist}-------------------")
         choose_test(testing, n, order)
 
 # function to choose the test
 def choose_test(testing, n, order):
+    """
+    Choose the appropriate test based on the algorithm and subject.
 
+    Args:
+        testing (Testing): The Testing object.
+        n (int): The number of times the test should be run.
+        order (str): The sorting order to be used.
+
+    Preconditions:
+        - testing must be a valid Testing object.
+        - n must be a positive integer.
+        - order must be a valid sorting order name.
+
+    """
+    # define algorithm
     algorithm = testing.algorithm
-    netlist = testing.netlist
 
+    # change order to random if order_choice is random
     if testing.order_choice == "random":
         testing.order = "random"
 
@@ -272,35 +438,50 @@ def choose_test(testing, n, order):
         # test all
         else:
             testing.testing_order(algorithm)
+
+        # print info to find output
         print(f"\nFind output in output/{testing.subject}")
+    
     # test algorithms
     else:
+
         # test the algorithms that dont give the same answer
+        # random
         if algorithm == "random2D":
             testing.average(n,"random2D")
+
+            # output histogram
             filepath = testing.get_filepath(testing.title) + ".csv"
             make_histogram(filepath, algorithm, n, order)
 
+        # random2D
         elif algorithm == "random":
             testing.average(n,"random")
-            print("here again")
+
+            # output histogram
             filepath = testing.get_filepath(testing.title) + ".csv"
             make_histogram(filepath, algorithm, n, order)
-
+        
+        # hillclimber
         elif algorithm == "hillclimber":
             testing.hillclimber(n,"hillclimber")
 
+        # annealing
         elif algorithm == "annealing":
             testing.hillclimber(n,"annealing")
 
         # test the algorithms that do give the same answer
+        # astar
         elif algorithm == "astar":
             testing.only_once("astar", order)
 
+        # greedy
         elif algorithm == "greedy":
             testing.only_once("greedy", order)
 
+        # else inform that function does not exist
         else:
             print("function not available")
 
+        # print info for output
         print(f"Find output in output/{testing.subject}/{algorithm}")
--- Code/algorithms/hillclimber.py
+++ Code/algorithms/hillclimber.py
@@ -1,116 +1,205 @@
 ###################################################
-# Hill Climber's algorithm
+# Hill Climber's algorithm with 
+# add-on simulated annealing
 ###################################################
+# import from libraries
 import sys
 import math
 import random
 import os
 import csv
+
+# import algorithms
 from randomize import random_add_wire
 from greedy import greedy_algorithm
 from astar import *
 from csv import writer
+
+# import classes
 sys.path.append("../Classes")
 from gate import *
 from location import *
 from wire import *
 from grid import *
+
+# import visualization
 sys.path.append("../Visualization")
 from graph import *
 from histogram import *
+
+# class hillclimber
 class Hillclimber:
     def __init__(self, chip, subject, n) -> None:
+        """
+        Initialize the Hillclimber object.
+
+        with Variables:
+            subject (str): The subject of the Hillclimber.
+            wire (Wire): The Wire object representing the wire being optimized.
+            chip (Chip): The Chip object representing the chip.
+            n (int): The number associated with the Hillclimber.
+            old_wire (list): The wire parts of the wire before optimization.
+            old_cost (int): The cost of the chip before optimization.
+            new_cost (int): The cost of the chip after optimization.
+            possibilities (list): The possible movement directions for optimization.
+            cost_list (list): The list of costs during optimization.
+            title (str): The title of the Hillclimber for file output.
+        """
+
+        # get the hilclimber info
         self.subject = subject
         self.wire: object
         self.chip = chip
         self.n = n
         
+        # initialize empty class variables
         self.old_wire = []
         self.old_cost = 0
         self.new_cost = 0
-        self.possibilities = [[0, 0, 1], [0, 1, 0], [1, 0, 0], [-1, 0, 0], [0, -1, 0], [0, 0, -1]]
         self.cost_list = []
         self.title = f"{chip.netlist}_{self.subject}_{n}"
 
+        # initialize possibilities
+        self.possibilities = [[0, 0, 1], [0, 1, 0], [1, 0, 0], [-1, 0, 0], [0, -1, 0], [0, 0, -1]]
+
+    # make new wire
     def start_wire(self, connection):
+        """
+        Set up the wire to be optimized.
+
+        Args:
+            connection (tuple): The gate connection associated with the wire.
+        """
         # get gate a and gate b
         gate_a = connection[0]
         gate_b = connection[1]
-        # print(gate_a, gate_b)
 
         # get the associated wire
         self.wire = self.chip.wires[f"{gate_a}-{gate_b}"]
-
+        
+        # save wire and calculate cost
         self.old_wire = self.wire.wireparts
         self.old_cost = self.chip.calculate_cost()
 
+    # make valid solution with greedy
     def start_with_greedy(self):
+        """
+        Start the optimization process with the greedy algorithm.
+        """
         greedy_algorithm(self.chip)
     
+    # make valid solution with astar
     def start_with_astar(self, version):
+        """
+        Start the optimization process with the A* algorithm.
+
+        Args:
+            version (str): The version of the A* algorithm to use.
+        """
         astar_algorithm(self.chip, version)
     
+    # check if change is better
     def check_is_better(self, i):
+        """
+        Check if the new cost is better than the old cost.
+
+        Args:
+            i (int): The index of the movement direction in the possibilities list.
+        """
+
+        # if cost is lower, make the change
         if self.new_cost <= self.old_cost: 
             self.old_wire = self.wire.wireparts
             self.old_cost = self.new_cost
             print("accept:", self.new_cost)
             self.cost_list.append(self.old_cost)
             self.write_to_csv()
-            
+
+        # else don't make the change  
         else:
             self.wire.wireparts = self.old_wire
-    def make_graph(self):
-        cost_list = get_info(self.filepath)
-        x = list(range(len(cost_list)))
-        graph(x,cost_list, self.title)
-    # output to csv
-    def make_csv(self, filepath):
-        
-        with open(filepath, 'w', newline='') as newfile:
-            writer = csv.writer(newfile, dialect='excel')
-            writer.writerow(["test", "cost"])
 
-    def write_to_csv(self):
-        filepath = f"output/algorithm/{self.subject}/{self.title}.csv"
-        
-        if not os.path.exists(filepath):
-            self.make_csv(filepath)
-
-        with open(filepath, 'a') as f_object:
-            # Pass this file object to csv.writer()
-            # and get a writer object
-            writer_object = writer(f_object)
-        
-            # Pass the list as an argument into
-            # the writerow()
-            writer_object.writerow(["new_cost: ",self.old_cost])
-        
-            # Close the file object
-            f_object.close()
-
+    # check for simulated annealing
     def check_is_better_annealing(self, start_t, i):
+        """
+        Check if the new cost is better than the old cost based on the acceptance probability in simulated annealing.
+
+        Args:
+            start_t (float): The initial temperature for simulated annealing.
+            i (int): The current iteration of the annealing process.
+        """
+        # calculate the accepting chance
         difference = self.old_cost - self.new_cost
         temperature = start_t*0.999999999**i
         accept = (2 ** difference)/temperature
         r = random.random()
+
+        # if accepting, make change
         if r < accept:
             self.old_wire = self.wire.wireparts
             self.old_cost = self.new_cost
             print("accept:", self.new_cost)
             self.cost_list.append(self.old_cost)
             make_graph(i, self.old_cost, self.title + "annealing")
+        # else don't make the change
         else:
             self.wire.wireparts = self.old_wire
-        
+
+    # create new wire and calculate cost 
     def make_new_wire(self):
+        """
+        Create a new wire for optimization and calculate its cost.
+        """
+        # set everything to 0
         self.new_cost = 0
         self.wire.wireparts = []
+
         # add random new wire and calculate new cost
         random_add_wire(self.possibilities, self.wire, self.chip)
         self.new_cost = self.chip.calculate_cost()
+    
+    # make a line graph
+    def make_graph(self):
+        """
+        Generate a graph of the cost during optimization.
+        """
+        cost_list = get_info(self.filepath)
+        x = list(range(len(cost_list)))
+        graph(x,cost_list, self.title)
+    
+    # output to csv
+    def make_csv(self, filepath):
+        """
+        Create a new CSV file.
 
+        Args:
+            filepath (str): The path of the CSV file to create.
+        """
+        with open(filepath, 'w', newline='') as newfile:
+            writer = csv.writer(newfile, dialect='excel')
+            writer.writerow(["test", "cost"])
 
+    # write one line to the csv
+    def write_to_csv(self):
+        """
+        Write the current cost to the CSV file.
+        """
+        filepath = f"output/algorithm/{self.subject}/{self.title}.csv"
+        
+        if not os.path.exists(filepath):
+            self.make_csv(filepath)
+
+        with open(filepath, 'a') as f_object:
+            # Pass this file object to csv.writer() and get a writer object
+            writer_object = writer(f_object)
+        
+            # Pass the list as an argument into the writerow()
+            writer_object.writerow(["new_cost: ",self.old_cost])
+        
+            # Close the file object
+            f_object.close()
+
+# main hillclimber algorithm
 def hillclimber_algorithm(chip:object, n):
     """
     Perform the hill climbing algorithm to optimize wire connections on a chip.
@@ -122,60 +211,83 @@ def hillclimber_algorithm(chip:object, n):
     Post-conditions:
         - The wire connections are optimized based on the hill climbing algorithm.
     """
-    # for every wire
+
+    # make hillclimber object
     i = 0
     hillclimber = Hillclimber(chip, "hillclimber", n)
+
+    # start with greedy algorithm
     hillclimber.start_with_greedy()
 
+    # randomly get a wire and change it via the random algorithm
     while True:
+        # shuffle connections
         random.shuffle(chip.wire_connections)
         
+        # go through shuffled list
         for connection in chip.wire_connections:
-
+            
+            # make new wire
             hillclimber.start_wire(connection)
 
             j = 0
-            # repeat
+            # continue to make new wires and check if the cost is better
             while True:
                 
+                # make and check
                 hillclimber.make_new_wire()
                 hillclimber.check_is_better(i)
                 j += 1
-                # repeat this ... times
-                if j == 20:
-                    # print("connected")
+
+                # repeat this 2 times
+                if j == 2:
                     break
         i += 1
-        # repeat this ... times
+        # repeat this n times
         if i == n:
             break
     
-
+# simulated annealing add-on
 def simulated_annealing(chip:object, n):
+    """
+    Perform the simulated annealing algorithm for optimizing wire connections in a chip.
+
+    Args:
+        chip (object): The chip object representing the circuit.
+        n (int): The number of iterations for the annealing process.
+    """
+    # make hillclimber object
     i = 0
     hillclimber = Hillclimber(chip, "annealing", n)
     hillclimber.start_with_greedy()
-    start_t = 1000
-    while True:
-        random.shuffle(chip.wire_connections)
-        
-        for connection in chip.wire_connections:
 
+    # define start
+    start_t = 1000
+
+    #randomly get a wire and change it via the random algorithm
+    while True:
+        # shuffle connections
+        random.shuffle(chip.wire_connections)
+        # go through shuffled list
+        # go through shuffled list
+        for connection in chip.wire_connections:
+            
+            # make new wire
             hillclimber.start_wire(connection)
 
             j = 0
-            # repeat
+            # continue to make new wires and check if the cost is accepted
             while True:
                 
+                # make and check
                 hillclimber.make_new_wire()
                 hillclimber.check_is_better_annealing(start_t, i)
                 j += 1
-                # repeat this ... times
+
+                # repeat this 2 times
                 if j == 2:
-                    # print("connected")
                     break
         i += 1
-        # repeat this ... times
+        # repeat this n times
         if i == n:
             break
-    return hillclimber.cost_list
